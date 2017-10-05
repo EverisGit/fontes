05de24d0e1e00a99769f7f078b2aed3f2a809346
evbudi <everisbrasiludi@gmail.com>
2017-10-05 00:40:11 -0300

    [SEGUNDO]

---
+++ ProjetoJava/src/br/com/everis/outro/ModuloOutro.java
@@ -0,0 +1,11 @@
+package br.com.everis.outro;
+
+public class ModuloOutro {
+	
+	private static final String MSGOUTRO = "#Modulo 2 executado";
+	
+	public String executa(){
+		return MSGOUTRO;
+	}
+
+}
--- ProjetoJava/src/br/com/everis/tecnologia/Principal.java
+++ ProjetoJava/src/br/com/everis/tecnologia/Principal.java
@@ -1,10 +1,14 @@
 package br.com.everis.tecnologia;
 
+import br.com.everis.outro.ModuloOutro;
+
 public class Principal {
 
 	public static void main(String[] args) {
 
 		System.out.println("#Inicio=Sistema#");
+		
+		System.out.println(new ModuloOutro().executa());
 		
 		System.out.println("#Fim====Sistema#");
 
